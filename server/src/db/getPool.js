4920ddf7138e57f194c8afa138930db9c8af703a
Ricardo Estupinan <ricardoea@hotmail.es>
2025-08-23 16:29:02 +0100

    refactor: update database connection and user model imports

--- server/src/db/getPool.js
+++ server/src/db/getPool.js
@@ -1,35 +1,38 @@
 
-import mysql from 'mysql/promise';
+import mysql from 'mysql2';
 
 const { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB } = process.env;
 
 let pool;
 
 const getPool = async () => {
-  try {
-    if (!pool) {
-      const dbConnection = await mysql.createConnection({
+  try
+  {
+    if ( !pool )
+    {
+      const dbConnection = await mysql.createConnection( {
         host: MYSQL_HOST,
         user: MYSQL_USER,
         password: MYSQL_PASS,
-      });
+      } );
 
       await dbConnection.query(
-        `CREATE DATABASE IF NOT EXISTS ${MYSQL_DB}`
+        `CREATE DATABASE IF NOT EXISTS ${ MYSQL_DB }`
       );
 
-      pool = mysql.createPool({
+      pool = mysql.createPool( {
         host: MYSQL_HOST,
         user: MYSQL_USER,
         password: MYSQL_PASS,
         database: MYSQL_DB,
         connectionLimit: 10,
         timezone: 'Z',
-      });
+      } );
     }
     return await pool;
-  } catch (err) {
-    console.error(err);
+  } catch ( err )
+  {
+    console.error( err );
   }
 };
 
--- server/src/models/users/insertUserModel.js
+++ server/src/models/users/insertUserModel.js
@@ -1,26 +1,28 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 import getPool from '../../db/getPool.js';
 import generateError from '../../utils/generateErrorUtil.js';
 
-const insertUserModel = async (username, email, password) => {
+const insertUserModel = async ( username, email, password ) => {
   const pool = await getPool();
 
-  let [users] = await pool.query(`SELECT id FROM users WHERE username = ?`, [username]);
-  if (users.length > 0) {
-    generateError('Nombre de usuario ya en uso', 409);
+  let [ users ] = await pool.query( `SELECT id FROM users WHERE username = ?`, [ username ] );
+  if ( users.length > 0 )
+  {
+    generateError( 'Nombre de usuario ya en uso', 409 );
   }
 
-  [users] = await pool.query(`SELECT id FROM users WHERE email = ?`, [email]);
-  if (users.length > 0) {
-    generateError('Email ya en uso', 409);
+  [ users ] = await pool.query( `SELECT id FROM users WHERE email = ?`, [ email ] );
+  if ( users.length > 0 )
+  {
+    generateError( 'Email ya en uso', 409 );
   }
 
-  const hashedPass = await bcrypt.hash(password, 10);
+  const hashedPass = await bcrypt.hash( password, 10 );
 
-  const [result] = await pool.query(
+  const [ result ] = await pool.query(
     `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`,
-    [username, email, hashedPass]
+    [ username, email, hashedPass ]
   );
   return result.insertId;
 };
