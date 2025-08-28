794acd1cf20c666fa3f670cc259c3b488a4ab4a6
Ricardo Estupinan <ricardoea@hotmail.es>
2025-08-28 13:39:00 +0100

    feat: mejorar formularios de inicio de sesión y registro, agregar manejo de errores y almacenamiento local

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,18 +1,35 @@
 
 
 const RegisterForm = () => {
+
+  const handleSubmit = (e: React.FormEvent) => {
+
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const newPassword = formData.get("confirmPassword");
+
+    // Perform registration action with username, email, password, and confirmPassword
+    console.log("Registering:", username, email, password, newPassword);
+
+    // Clear form fields after submission
+    (e.currentTarget as HTMLFormElement).reset();
+  }
+
   return (
-    <form className="bg-emerald-500/60 backdrop-blur-md p-8 rounded-4xl text-center w-3/4 md:w-1/2 lg:w-1/3 z-10 relative -top-44 ">
+    <form className="bg-emerald-500/60 backdrop-blur-md p-8 rounded-4xl text-center w-3/4 md:w-1/2 lg:w-1/3 z-10 relative -top-44 " onSubmit={handleSubmit}>
       <h1 className="text-4xl font-bold text-emerald-500 mb-4">Create Account</h1>
       <p className="mt-4 text-lg text-emerald-100 mb-5">Welcome to the Register page!</p>
 
-      <input type="text" className="border text-emerald-50 border-emerald-300 p-2 rounded-lg w-2/3 mb-4" placeholder="Username" />
+      <input type="text" name="username" className="border text-emerald-50 border-emerald-300 p-2 rounded-lg w-2/3 mb-4" placeholder="Username" autoComplete="username" />
 
-      <input type="email" className="border text-emerald-50 border-emerald-300 p-2 rounded-lg w-2/3 mb-4" placeholder="Email" />
+      <input type="email" name="email" className="border text-emerald-50 border-emerald-300 p-2 rounded-lg w-2/3 mb-4" placeholder="Email" autoComplete="email" />
 
-      <input type="password" className="border text-emerald-50 border-emerald-300 p-2 rounded-lg w-2/3 mb-4" placeholder="Password" />
+      <input type="password" name="password" className="border text-emerald-50 border-emerald-300 p-2 rounded-lg w-2/3 mb-4" placeholder="Password" autoComplete="new-password" />
 
-      <input type="text" className="border text-emerald-50 border-emerald-300 p-2 rounded-lg w-2/3 mb-4" placeholder="Confirm Password" />
+      <input type="password" name="confirmPassword" className="border text-emerald-50 border-emerald-300 p-2 rounded-lg w-2/3 mb-4" placeholder="Confirm Password" autoComplete="new-password" />
 
       <p className="text-sm text-emerald-100 mb-5 relative right-18">
         I agree to the <a href="/terms" className="underline hover:text-red-400 left-11">
