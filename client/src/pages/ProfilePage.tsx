b186a7bda41f66d04fca25e7f2e86d5e83fe9fa9
Ricardo Estupinan <ricardoea@hotmail.es>
2025-09-13 12:37:30 +0100

    feat: rediseñar la página de perfil para incluir el componente ProfileCard y mejorar la presentación

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,8 +1,15 @@
+import ProfileCard from "../components/ProfileCard";
+
+
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile</h1>
-      <p>Welcome to the Profile page!</p>
+    <div className="w-full min-h-screen flex flex-col items-center bg-emerald-500 z-0">
+      <div className="bg-[url('/bg-profile.jpg')] bg-cover bg-no-repeat h-full w-full opacity-80 flex-grow">
+        <div className="bg-emerald-500/50 backdrop-blur-xs min-h-screen flex items-center justify-center p-4">
+          <ProfileCard />
+        </div>
+
+      </div>
     </div>
   );
 }
